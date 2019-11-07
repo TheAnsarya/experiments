5a051a2bd043246b74d5552a163fc1f48b39b8ea
Andy Hubbard <Andy.Hubbard@kikoda.com>
2019-11-07 10:56:28 -0500

    Add project files.

---
+++ AutoCastReturnTypes/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace AutoCastReturnTypes {
+	class Program {
+		static void Main(string[] args) {
+			var exp = new Experiment();
+
+			Widget w = exp.GetEntity();
+			Doodad d = exp.GetEntity();
+		}
+	}
+
+	class Experiment {
+		public Doodad GetEntityAsDoodad() {
+			return new Doodad();
+		}
+
+		public Widget GetEntityAsWidget() {
+			return new Widget();
+		}
+
+		public class GetEntityAuto {
+			private Experiment experiment;
+
+			public GetEntityAuto(Experiment experiment) {
+				this.experiment = experiment;
+			}
+
+			public static implicit operator Widget(GetEntityAuto value) {
+				return value.experiment.GetEntityAsWidget();
+			}
+
+			public static implicit operator Doodad(GetEntityAuto value) {
+				return value.experiment.GetEntityAsDoodad();
+			}
+		}
+
+		public GetEntityAuto GetEntity() {
+			return new GetEntityAuto(this);
+		}
+	}
+
+	class Widget {
+	}
+
+	class Doodad {
+	}
+}
